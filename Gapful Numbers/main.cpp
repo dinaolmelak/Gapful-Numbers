1ee05cb901008726fb005744013e3fd99a63878f
Dinaol Tadesse <dinaolmelak@gmail.com>
2018-11-16 01:18:24 -0600

    Started with an Algorithm

--- Gapful Numbers/main.cpp
+++ Gapful Numbers/main.cpp
@@ -5,11 +5,24 @@
 //  Created by Dinaol Melak on 11/16/18.
 //  Copyright © 2018 Dinaol Melak. All rights reserved.
 //
-
+//  This is a Solo Learn Challenge of a program
+//  The program is to identify whether or not a number is a Gapful Number
+//      Gapful Number:- is a number of at least 3 digits that is divisible by the number
+//                      formed by the first and last difit of the original number
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    // Algorithm to Solve this problem
+    /*
+     - first we get a number from a user
+     - then we change the number to a string
+     - then we store the first char with the last char of the string
+     - change the char to a string and divide the original number with this string
+        - if it has no remainder, then return true
+        - else return false
+     
+     
+     */
     return 0;
 }
